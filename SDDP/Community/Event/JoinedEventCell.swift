0e0c65db9fd2101a4b80e0dcb883e80b4471a2f7
hhf <hhf@hhfs-iMac.local>
2018-07-31 19:01:54 -0700

    lala

---
+++ SDDP/Community/Event/JoinedEventCell.swift
@@ -0,0 +1,9 @@
+//
+//  JoinedEventCell.swift
+//  SDDP
+//
+//  Created by hhf on 31/7/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+import Foundation
---
+++ SDDP/Entity/Event.swift
@@ -0,0 +1,9 @@
+//
+//  Event.swift
+//  SDDP
+//
+//  Created by hhf on 30/7/18.
+//  Copyright © 2018 ITP312. All rights reserved.
+//
+
+import Foundation
